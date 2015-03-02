a57693474ce193dba01b8a8e0b2527241ab655f0
渔夫 <vzheng56@gmail.com>
2015-03-02 11:21:00 +0800

    add priject
    first add！

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public enum NumScoreLevel {
+		ToFar = 0,
+		Near,
+		Correct
+	}
+
+	public NumScoreLevel currentScoreLevel = NumScoreLevel.ToFar;
+	private int currentNumber = 0;
+	public int ProduceGameNumber() {
+		currentNumber = Random.Range (0, 100);
+		return  currentNumber;
+	}
+
+	public NumScoreLevel CompareNumber(int guessNumber){
+		if (Mathf.Abs (guessNumber - currentNumber) > 5) {
+			currentScoreLevel = NumScoreLevel.ToFar;
+		} else if (Mathf.Abs (guessNumber - currentNumber) <= 5&&Mathf.Abs (guessNumber - currentNumber)>0) {
+			currentScoreLevel = NumScoreLevel.Near;
+		} else {
+			currentScoreLevel = NumScoreLevel.Correct;
+		}
+		return currentScoreLevel;
+	}
+
+	                                
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	public Text currentValueText;
+	public Text guessNumberText;
+	public Text lastConpareInfoText;
+	public Text scoreText;
+
+	public GameLogic GameLogicController;
+	private int currentScore = 0;
+
+	void Start() {
+		scoreText.text = "0";
+		StartCoroutine (ChangeNumberText ());
+	}
+
+	private int guessNumber = 50;
+	public void SliderValueChanged(float value){
+		if (currentValueText) {
+			guessNumber = (int)value;
+			currentValueText.text = "CurrentValue: "+value.ToString("0");
+		}
+	}
+
+	public void CompareNumberAction() {
+
+		switch (GameLogicController.CompareNumber (guessNumber)) {
+		case GameLogic.NumScoreLevel.ToFar:
+			lastConpareInfoText.text = "cha de tai duo le ";
+			currentScore += 10;
+			break;
+		case GameLogic.NumScoreLevel.Near:
+			lastConpareInfoText.text = "bu cuo ,cha dian cai dui  ";
+			currentScore += 100;
+			break;
+		case GameLogic.NumScoreLevel.Correct:
+			lastConpareInfoText.text = "wan quan zheng que!";
+			currentScore += 200;
+			break;
+		}
+		scoreText .text ="Score: "+ currentScore.ToString ();
+		StartCoroutine (ChangeNumberText ());
+	}
+
+	IEnumerator ChangeNumberText()
+	{
+
+		yield return new WaitForSeconds (3);
+		var currentNumber = GameLogicController.ProduceGameNumber ();
+		guessNumberText.text = "Guess: " + currentNumber;
+		lastConpareInfoText.text = "nuo dong dao ni cai de  wei zhi";
+	}
+
+}
